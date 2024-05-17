da33bf4ceedf8791c4b539f5d424c5a37d47e63d
mtkennerly <mtkennerly@gmail.com>
2024-05-17 00:16:08 -0400

    Mark <home>/Library as too broad

--- src/path.rs
+++ src/path.rs
@@ -85,6 +85,7 @@ fn too_broad(path: &str) -> bool {
         format!("{HOME}/Library/Application Support/UserData"),
         format!("{HOME}/Library/Preferences"),
         format!("{HOME}/.renpy"),
+        format!("{HOME}/Library"),
         format!("{HOME}/Library/RenPy"),
         format!("{HOME}/Telltale Games"),
         format!("{ROOT}/config"),
